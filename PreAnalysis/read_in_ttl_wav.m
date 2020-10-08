cb62b143a6f2fb76fbe14dfd91856cc271ab01ca
MadeleineSnyder <madeleine.snyder.48@gmail.com>
2020-10-08 11:43:17 -0700

    chagning folder structure

--- PreAnalysis/read_in_ttl_wav.m
+++ PreAnalysis/read_in_ttl_wav.m
@@ -5,11 +5,12 @@ bat1 = 'Clementine'; % Silver Falcon
 bat2 = 'Ovaltine'; % Optimus Prime
 bat3 = 'Saltine'; 
 bat4 = 'Valentine';
-day = '200923'; % Date [yrmoda]
-paradigm = 'FeedTask';
+day = '200930'; % Date [yrmoda]
+paradigm = 'RecOnly';
 HLRBat = 'Ovaltine';
-session = '2';
-root_save = ['C:' filesep 'Users' filesep 'Batman' filesep 'Desktop' filesep 'BatHumanExp' filesep 'box_3' filesep day filesep paradigm filesep strcat('session',session)];
+session = '1';
+root_save = ['C:' filesep 'Users' filesep 'Batman' filesep 'Desktop' filesep 'BatHumanExp' filesep 'box_3' filesep day filesep strcat('session',session)];
+root_save_dca = ['C:' filesep 'Users' filesep 'Batman' filesep 'Desktop' filesep 'BatHumanExp' filesep 'box_3' filesep day];
 
 %% Examine the ttl WAV
 % Find the ttl files you want
@@ -17,6 +18,8 @@ if strcmp(paradigm,'FeedTask')
     filetype = strcat(bat1(1:2),bat2(1:2),bat3(1:2),bat4(1:2),'_',day,'*',paradigm,'*','ttl','*');
 elseif strcmp(paradigm,'HLRTask')
     filetype = strcat(HLRBat,'_',day,'*',paradigm,'*','ttl','*');
+elseif strcmp(paradigm,'RecOnly')
+    filetype = strcat(bat1(1:2),bat2(1:2),bat3(1:2),bat4(1:2),'_',day,'*',paradigm,'*','ttl','*');
 end  
 dirlist = dir(fullfile(root_save,filetype)); 
 ttl_wav = [root_save filesep dirlist(end).name];
@@ -30,8 +33,6 @@ dt = 1/Fs_ttl;
 t_ttl = 0:dt:(length(y_ttl)*dt)-dt;
 figure(); hold on;
 title('Raw TTL Pulse');plot(t_ttl(1:end),y_ttl(1:end)); xlabel('Seconds'); ylabel('Amplitude');
-%figure(); hold on;
-%plot(psd(spectrum.periodogram,y_ttl,'Fs',Fs_ttl,'NFFT',length(y_ttl)));
 
 %% Examine the actual audio WAV from mic1
 
@@ -40,6 +41,8 @@ if strcmp(paradigm,'FeedTask')
     filetype = strcat(bat1(1:2),bat2(1:2),bat3(1:2),bat4(1:2),'_',day,'*',paradigm,'*','mic1','*');
 elseif strcmp(paradigm,'HLRTask')
     filetype = strcat(HLRBat,'_',day,'*',paradigm,'*','mic1','*');
+elseif strcmp(paradigm,'RecOnly')
+    filetype = strcat(bat1(1:2),bat2(1:2),bat3(1:2),bat4(1:2),'_',day,'*',paradigm,'*','mic1','*');
 end
 dirlist = dir(fullfile(root_save,filetype)); 
 mic1_wav = [root_save filesep dirlist(end).name];
@@ -86,18 +89,18 @@ figure(); hold on;
 title('Raw Mic1 Amplitude and TTL and DCA');
 plot(t_mic1,y_mic1); xlabel('Seconds'); ylabel('Amplitude');
 plot(t_ttl(1:end),y_ttl(1:end)); xlabel('Seconds'); ylabel('Amplitude');
-plot(t_dca(1:end),y_dca(1:end)); xlabel('Seconds'); ylabel('Amplitude');
+%plot(t_dca(1:end),y_dca(1:end)); xlabel('Seconds'); ylabel('Amplitude');
 
 % Plot the end of the recording to make sure the recoding GUI was stopped
 % last. 
 figure(); hold on; 
-plot(t_mic1(end-1000:end),y_mic1(end-1000:end)); xlabel('Seconds'); ylabel('Amplitude');
-plot(t_ttl(end-1000:end),y_ttl(end-1000:end)); xlabel('Seconds'); ylabel('Amplitude');
-plot(t_dca(end-1000:end),y_dca(end-1000:end)); xlabel('Seconds'); ylabel('Amplitude');
+plot(t_mic1(end-100000:end),y_mic1(end-100000:end)); xlabel('Seconds'); ylabel('Amplitude');
+plot(t_ttl(end-100000:end),y_ttl(end-100000:end)); xlabel('Seconds'); ylabel('Amplitude');
+%plot(t_dca(end-1000:end),y_dca(end-1000:end)); xlabel('Seconds'); ylabel('Amplitude');
 
 
 %% Read in the decawave positional data and plot it below 
-deca = load(strcat(root_save,filesep,'decawave_',day,'_',session,'.mat'));
+deca = load(strcat(root_save_dca,filesep,'decawave_',day,'_',session,'.mat'));
 figure(); hold on;
 title('Decawave Data');
 scatter3(deca.UD.Pos(1,1,:),deca.UD.Pos(2,1,:),deca.UD.Pos(3,1,:),'r');
